4d0bdf533399e9f9b3f68fe08343e14b098f6876
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-03-17 09:46:30 +0300

    my django with database

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,7 +8,9 @@ class Book(models.Model):
     Books data
     """
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=True)
     
     def __str__(self):
         """ format string to Return how ever we want
