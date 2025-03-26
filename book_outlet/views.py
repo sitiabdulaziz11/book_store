985dbb1d865ab822a656396b6cde62bb117cdf77
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-03-26 09:37:43 +0300

    data base with django

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+
+def index(request):
+    """base page
+    """
+    book = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": book
+    })
