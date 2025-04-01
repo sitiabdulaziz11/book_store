66943cef2d17a81efd3964a482e5fc82eb7e6a48
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-01 22:18:40 +0300

    data base with django

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -11,7 +12,19 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=True)
+    slug = models.SlugField(default="", null=False)  # if the title is Harry Potter 1 => the slug could be like this harry-potter-1
     
+    def get_absolute_url(self):
+        """To get url
+        """
+        return reverse("book-detail", args=[self.id])
+    
+    def save(self, *args, **kwargs):
+        """user defined ssave method.
+        """
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
     def __str__(self):
         """ format string to Return how ever we want
         """
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 
@@ -11,3 +12,19 @@ def index(request):
     return render(request, "book_outlet/index.html", {
         "books": book
     })
+
+def book_detail(request, id):
+    """Views which gives detail of the book.
+    """
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)   # this is alternative of above try and except part
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
+    
