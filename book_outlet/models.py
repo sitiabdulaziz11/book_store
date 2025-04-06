c94718dd1044c45633d539119c1d55c430e9da58
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-04-06 15:43:34 +0300

    django project

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,13 +4,28 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+
 class Author(models.Model):
     """Authors model.
     """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    Address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
     
-
+    def full_name(self):
+        """To display full name
+        """
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name
+    
 class Book(models.Model):
     """
     Books data
@@ -18,7 +33,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     # author = models.CharField(null=True, max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=True)
     # slug = models.SlugField(default="", blank=True,editable=False, null=False, db_index=True)  # if the title is Harry Potter 1 => the slug could be like this harry-potter-1
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)  # if the title is Harry Potter 1 => the slug could be like this harry-potter-1
